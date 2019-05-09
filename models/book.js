45b940c2986b12f9e03464bc138100b1c098970f
Ivan5 <converse_ivan@hotmail.com>
2019-05-09 15:34:22 -0500

    Refactor some code in this section

--- models/book.js
+++ models/book.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const path = require("path");
 
-const coverImageBasePath = "uploads/bookCover";
+const coverImageBasePath = "uploads/bookCovers";
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -31,6 +32,12 @@ const bookSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "Author"
+  }
+});
+
+bookSchema.virtual("coverImagePath").get(function() {
+  if (this.coverImageName != null) {
+    return path.join("/", coverImageBasePath, this.coverImageName);
   }
 });
 
