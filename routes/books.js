dd408b43b87f133d75f8e9045861cda28f9841f3
Ivan5 <converse_ivan@hotmail.com>
2019-05-09 14:49:45 -0500

    Add multer for file upload

--- routes/books.js
+++ routes/books.js
@@ -1,30 +1,57 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 const Book = require("../models/book");
 const Author = require("../models/author");
-
+const uploadPath = path.join("public", Book.coverImageBasePath);
+const imageMineTypes = ["images/jpeg", "images/png", "images/gif"];
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null);
+  }
+});
 //All Book Route
 router.get("/", async (req, res) => {
   res.send("All books");
 });
 
 //New Book routes
-router.get("/new", async (req, res) => {
-  try {
-    const authors = await Author.find();
-    const book = new Book();
-    res.render("books/new", {
-      authors: authors,
-      book: book
-    });
-  } catch (e) {
-    res.redirec("/books");
-  }
+router.get("/new", upload.single("cover"), async (req, res) => {
+  renderNewPage(res, new Book());
 });
 
 //Create Book Route
 router.post("/", async (req, res) => {
-  res.send("Create Book");
+  const fileName = req.file != null ? req.file.filename : null;
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    coverImageName: fileName,
+    description: req.body.description
+  });
+  try {
+    const newBook = await book.save();
+    res.redirec("books");
+  } catch (e) {
+    renderNewPage(res, book, true);
+  }
 });
 
+async function renderNewPage(res, book, hasError = false) {
+  try {
+    const authors = await Author.find({});
+    const params = {
+      authors: authors,
+      book: book
+    };
+    if (hasError) params.errorMessage = "Error Creating Book";
+    res.render("books/new", params);
+  } catch (e) {
+    res.redirec("/books");
+  }
+}
 module.exports = router;
