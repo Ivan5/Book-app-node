715ba2462707e6fca52a418373fa8a03aeed8010
Ivan5 <converse_ivan@hotmail.com>
2019-05-13 18:09:55 -0500

    Add update function

--- routes/authors.js
+++ routes/authors.js
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
   });
   try {
     const newAuthor = await author.save();
-    res.redirect("authors");
+    res.redirect(`authors/${newAuthor.id}`);
   } catch (e) {
     res.render("authors/new", {
       author,
@@ -49,8 +49,23 @@ router.get("/:id/edit", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  res.send("Update author " + req.params.id);
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch (e) {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.render("authors/edit", {
+        author,
+        errorMessage: "Error updating author"
+      });
+    }
+  }
 });
 
 router.delete("/:id", (req, res) => {
